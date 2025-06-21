bcf1b7c3f8802604744923d19dc532aee2398812
serverxdt <runningserver94@gmail.com>
2025-06-22 02:35:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,174 +10,44 @@ app.debug = True
 html_content = '''
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Henry Server</title>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            font-family: Popins, sans-serif;
-            background-color: cream;
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-position: center;
-            margin: 0;
-            padding: 0;
-        }
-        .container {
-            max-width: 50px auto; /* Decreased max-width */
-            margin: 50px auto; /* Adjusted margin */
-            padding: 20px;
-            background-color: rgba(220, 220, 220, 0.5); /* Transparent white background */
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            border-radius: 8px;
-        }
-        h1 {
-            text-align: center;
-            color: white;
-            border: 1.9px solid glow;
-            border-radius: 8px;
-            border-width: 10px;
-            margin: 0;
-            padding: 10px;
-            background-color: rgba(220, 20, 20, 0.5); /* Transparent red background */
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-        h2 {
-            color: #fff;
-            font-size: 20px;
-            margin-bottom: 20px;
-            text-shadow: 0 0 10px #000;
-        }
+  <!-- Project Name: H3NR!'W - APP
+Copyright: �Shahzaib_Khanzada -->
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>H3NRI'W - SERVERS</title>
+    <!-- FontAwesome CDN -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    />
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="/home/main.css" />
+  </head>
+  <body>
+    <div class="container">
+      <!-- APP cards -->
+      <div class="movie-cards">
 
-        /* Blinking Sukhi Server heading */
-        .sukhi-server {
-            font-size: 32px;
-            color: #ff5e5e;
-            animation: blink 1.5s infinite;
-            font-weight: bold;
-            margin-bottom: 20px;
-        }
-
-        @keyframes blink {
-            0%, 100% {
-                opacity: 1;
-            }
-            50% {
-                opacity: 0;
-            }
-        }
-
-        input {
-            width: 100%;
-            padding: 12px;
-            margin: 10px 0;
-            border-radius: 8px;
-            border: 1px solid #ccc;
-            font-size: 16px;
-            background-color: rgba(255, 255, 255, 0.9);
-        }
-
-        form {
-        display: flex;
-        flex-direction: column; /* Arrange children in a column */
-        align-items: center;    /* Center items horizontally */
-        }
         
-        button {
-        width: auto;            /* Change to auto for centered width */
-        padding: 12px 20px;     /* Adjust padding for better appearance */
-        background-color: #007bff;
-        color: #fff;
-        border: none;
-        cursor: pointer;
-        border-radius: 8px;
-        margin-top: 15px;
-        font-weight: bold;
-        font-size: 16px;
-        transition: background-color 0.3s ease;
-        }
-
-        button:hover {
-            background-color: #0056b3;
-        }
-
-        .admin-contact {
-            margin-top: 20px;
-            color: #fff;
-        }
-
-        .admin-contact a {
-            color: #00ff00;
-            font-weight: bold;
-            text-decoration: none;
-        }
-
-        .error-message {
-            color: red;
-            font-size: 14px;
-            margin-top: 10px;
-            font-weight: bold;
-        }
-        #check {display: none;}
-        @media(max-width: 610px){
-       .menu {
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 80%;
-        height: 100vh;
-        position: fixed;
-        top: 0;
-        right: -100%;
-        z-index: 100;
-        background-color: var(--color2);
-        transition: all 0.2s ease-in-out;
-    </style>
-</head>
-<body>
-    <header>
-    </nav>
-    </header>
-    <br />
-    <h2>𝗚𝗘𝗧 𝗙𝗥𝗘𝗘 𝗦𝗘𝗥𝗩𝗘𝗥 ➤ 𝙃𝙚𝙣𝙧𝙮 𝙓 𝙎𝙖𝙢𝙖𝙧</h2>
-    <br />
-    
-               <div class="content">
-        <body>
-  <video class="video" autoplay loop muted>
-    <source src=("https://i.imgur.com/7GUAWWz.gif") type="video/mp4">
-  </video>
-  </body>
- <h1>╰┈➤ 🩷 𝗙𝗿𝗲𝗲 𝗦𝗲𝗿𝘃𝗲𝗿 𝗔𝗽𝗽 𝗕𝘆 𝗛𝗲𝗻𝗿𝘆 𝗫 𝗦𝗮𝗺𝗮𝗿 ᯓ★</h1>
- <br />
- <button class="button-34" role="button" onclick="window.location.href='https://www.mediafire.com/file/dotpunuezc9ji5p/HENRY+X+SAMAR+APK+.apk/file'">⊲ DOWNLOAD ⊳</button>
-    <br />
-    <br />
-
-    <div class="footer">
-    <div class="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
-      <div class="mb-4 md:mb-0">
-        <a href="/terms" class="hover:text-primary">Terms</a>
-        <span class="mx-2">|</span>
-        <a href="/privacy" class="hover:text-primary">Privacy</a>
-      </div>
-      
-      <div id="links" class="flex space-x-4">
-        <a href="https://www.facebook.com/henry.inxide" class="text-2xl hover:text-primary"><i class="fab fa-facebook"></i></a>
-        <a href="https://wa.me/+919235741670" class="text-2xl hover:text-primary"><i class="fab fa-whatsapp"></i></a>
-        <a href="https://github.com/Henryinxid3/" class="text-2xl hover:text-primary"><i class="fab fa-github"></i></a>
-      </div>
-      
-      <div class="mt-4 md:mt-0 text-center">
-        <p>© 2024 Henry Dwn. All Rights Reserved.</p>
-        <p>Made with ❤️ by <a href="">HENRY INXIDE</a></p>
-      </div>
-        <br />
-    </div>
+        <!-- Card 02 -->
+        <div class="card">
+          <img src="/poster2.jpg" alt="H3NRY T00L" />
+          <div class="content">
+            <h1 class="name">H3NRY'X</h1>
+            <h3 class="infos">
+              <i class="fa-solid fa- "></i> 2024-11-10 | Status:Trail
+            </h3>
+            <p class="short-desc">
+              This is the Free Trail of (H3NRY'X) Click (START) Button & USING (H3NRY'X) App & Get  Free 1 Server only You can Run Unlimmited IDS & Multi File on this server GOD Abusers Is'nt Allowed on WEB | Religon Abusers | GOD Abusers | Tokens Claimed and Fucked By Owner: H3NRY'X.            </p>
+            <div class="icons">
+              <a href="https://page-server-xg7c.onrender.com/"><i class="fa-solid fa-START"></i></a>
+              <a href="#"><i class="fa-solid fa-water"></i></a>
+              <a href="#"><i class="fa-solid fa-next"></i></a>
+            </div>
+          </div>
+        </div>
+      <div 
 </body>
 </html>
 '''
