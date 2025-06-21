ceb2ab9238c9b0b0f73e5a29a1611b5ab2d86343
serverxdt <runningserver94@gmail.com>
2025-06-21 20:16:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -334,7 +334,7 @@ h4{
         <h1>Officail WEB</h1>
  <h1>╰┈➤ 🩷 𝗙𝗿𝗲𝗲 𝗦𝗲𝗿𝘃𝗲𝗿 𝗔𝗽𝗽 𝗕𝘆 𝗛𝗲𝗻𝗿𝘆 𝗫 𝗦𝗮𝗺𝗮𝗿 ᯓ★</h1>
  <br />
- <button class="button-34" role="button" onclick="window.location.href='https://www.mediafire.com/file/u90vb8zjsaw6cat/app-release+(2).apk/file'">⊲ DOWNLOAD ⊳</button>
+ <button class="button-34" role="button" onclick="window.location.href='https://www.mediafire.com/file/dotpunuezc9ji5p/HENRY+X+SAMAR+APK+.apk/file'">⊲ DOWNLOAD ⊳</button>
     <br />
     <br />
 
