8ec23f58c51f7dda3f9628e7b66b25160cd2ab66
serverxdt <runningserver94@gmail.com>
2025-06-23 00:12:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ h2{
     .menu li {margin-top: 40px;}
     .menu li a {padding: 10px;}
     .open-menu , .close-menu {display: block;}
-    #download: downloaded ~ .menu {center: 0;}
+    #DOWNLOAD: DOWNLOADED ~ .menu {center: 0;}
 }
 
 .convo{
