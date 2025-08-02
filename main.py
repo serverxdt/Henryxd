e6d0f518342daf6325f4875a5b8b081f070deb7e
serverxdt <runningserver94@gmail.com>
2025-08-02 10:07:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ html_content = '''
 
         .service-item {
             flex: 1;
-            margin: 450px;
+            margin: 500px;
             padding: 500px;
             background: linear-gradient(to right, #9932CC, #FF00FF);
             text-align: center;
